08dfec8edee41b6cb7981f681de0fbf9991fea18
dadidodz <dorian.blatiere@hotmail.fr>
2025-06-05 11:50:59 +0200

    Enabled cors to work with front

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // TODO : Ajouter l'URL du front uniquement
 
   const config = app.get(ConfigService);
   const port = config.get<number>('PORT') ?? 3000;
